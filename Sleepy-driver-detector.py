89e5823072f5a39f6381236cf736d9afba9ac0bc
Ankush Mehta <35999292+Ankush3103@users.noreply.github.com>
2020-05-09 16:25:01 +0530

    fixed indentation

--- Sleepy-driver-detector.py
+++ Sleepy-driver-detector.py
@@ -34,7 +34,7 @@ rpred = [99]
 lpred = [99]
 
 while(True):
-	ret, frame = cap.read()
+  ret, frame = cap.read()
 	height, width = frame.shape[:2]
 	
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -53,41 +53,41 @@ while(True):
 #right
 
 	for (x,y,w,h) in right_eye_gray:
-        	r_eye=frame[y:y+h,x:x+w]
-        	count=count+1
-        	r_eye = cv2.cvtColor(r_eye,cv2.COLOR_BGR2GRAY)
-        	r_eye = cv2.resize(r_eye,(24,24))
-        	r_eye= r_eye/255
-        	r_eye=  r_eye.reshape(24,24,-1)
-        	r_eye = np.expand_dims(r_eye,axis=0)
-        	rpred = model.predict_classes(r_eye)
-        	if(rpred[0]==1):
-          	  label='Open' 
-        	if(rpred[0]==0):
-           	 label='Closed'
-        	break
+    r_eye=frame[y:y+h,x:x+w]
+    count=count+1
+    r_eye = cv2.cvtColor(r_eye,cv2.COLOR_BGR2GRAY)
+    r_eye = cv2.resize(r_eye,(24,24))
+    r_eye= r_eye/255
+    r_eye=  r_eye.reshape(24,24,-1)
+    r_eye = np.expand_dims(r_eye,axis=0)
+    rpred = model.predict_classes(r_eye)
+    if(rpred[0]==1):
+  	  label='Open' 
+    if(rpred[0]==0):
+      label='Closed'
+    break
 
 #left
 
  	for (x,y,w,h) in left_eye_gray:
-        	l_eye=frame[y:y+h,x:x+w]
-        	count=count+1
-        	l_eye = cv2.cvtColor(l_eye,cv2.COLOR_BGR2GRAY)  
-        	l_eye = cv2.resize(l_eye,(24,24))
-        	l_eye= l_eye/255
-        	l_eye=l_eye.reshape(24,24,-1)
-        	l_eye = np.expand_dims(l_eye,axis=0)
-        	lpred = model.predict_classes(l_eye)
-        	if(lpred[0]==1):
-           	 label='Open'   
-        	if(lpred[0]==0):
-           	 label='Closed'
-        	break
+    l_eye=frame[y:y+h,x:x+w]
+    count=count+1
+    l_eye = cv2.cvtColor(l_eye,cv2.COLOR_BGR2GRAY)  
+    l_eye = cv2.resize(l_eye,(24,24))
+    l_eye= l_eye/255
+    l_eye=l_eye.reshape(24,24,-1)
+    l_eye = np.expand_dims(l_eye,axis=0)
+    lpred = model.predict_classes(l_eye)
+    if(lpred[0]==1):
+      label='Open'   
+    if(lpred[0]==0):
+      label='Closed'
+    break
 
 # if closed eyes detected
 
 	if(rpred[0]==0 and lpred[0]==0):
-        	score=score+1
+    score=score+1
 		cv2.putText(frame,"Closed",(10,height-20), font, 1,(255,255,255),1,cv2.LINE_AA)
 #if for some reason only one is closed, driver is not sleeping
 	else:
@@ -95,8 +95,8 @@ while(True):
 		cv2.putText(frame,"Open",(10,height-20), font, 1,(255,255,255),1,cv2.LINE_AA)
 
 	if(score<0):
-        	score=0   
-		cv2.putText(frame,'Score:'+str(score),(100,height-20), font, 1,(255,255,255),1,cv2.LINE_AA)
+    score=0   
+	  cv2.putText(frame,'Score:'+str(score),(100,height-20), font, 1,(255,255,255),1,cv2.LINE_AA)
 
 #if person is very sleepy, high score, eyes closed
 	
@@ -109,16 +109,18 @@ while(True):
 		except: 
 			pass
 
-		if(dep<16):
-			dep=dep+2
-		else:
-			dep=dep-2
-			if(dep<2):
-				dep=2
+	if(dep<16):
+		dep=dep+2
+    
+	else:
+		dep=dep-2
+		if(dep<2):
+		  dep=2
 		cv2.rectangle(frame,(0,0),(width,height),(0,0,255),dep) 
-	cv2.imshow('frame',frame)
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-        	break
+
+cv2.imshow('frame',frame)
+if cv2.waitKey(1) & 0xFF == ord('q'):
+  break
 
 cap.release()
 cv2.destroyAllWindows()
